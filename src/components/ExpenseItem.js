225fbc733489a26a4746298da54ba755ed2f7c13
AmanRajVerma10 <verma.aman464@gmail.com>
2023-10-06 17:34:04 +0530

    reactFirst

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <h2>Expense Item</h2>
+            <div>Food Rs 10</div>
+            <div>Petrol Rs 100</div>
+            <div>Movie Rs 200</div>
+        </div>
+        
+    )
+
+}
+
+export default ExpenseItem;
